66945ac0d1663887dd89bf90542b7e49ae6a0eca
najmus2808 <najmus2808@gmail.com>
2021-11-06 14:30:08 +0600

    add HoverCounter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ClickCounter from "./components/ClickCounter";
+import HoverCounter from "./components/HoverCounter ";
 
 const App = () => {
   return (
     <div>
       <ClickCounter />
+      <HoverCounter />
     </div>
   );
 };
